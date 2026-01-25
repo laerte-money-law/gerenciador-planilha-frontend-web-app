d785bda4fa00dee7c01d7955a2751a0209cd55b3
Maycon Huanca <mayconfelipe164@gmail.com>
2026-01-25 20:09:59 -0300

    feat(planilhas): remove unused requests and components

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -43,10 +43,6 @@ export class AuthService {
         return this.http.post<any>(AppUrls.API_ENDPOINTS.AUTH.LOG_IN(), data);
     }
 
-    getDashboardSummary(): Observable<any> {
-        return this.http.get<any>(AppUrls.API_ENDPOINTS.ADMIN.DASHBOARD_SUMMARY());
-    }
-
     logout(): void {
         localStorage.removeItem(AuthService.ACCESS_TOKEN);
         localStorage.removeItem(AuthService.REFRESH_TOKEN);
