0170e35a02f1efa7634514a3ce569c185deb4841
Maycon Huanca <mayconfelipe164@gmail.com>
2026-02-04 20:32:12 -0300

    feat: ajustar pipelines e variaveis

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: "https://money-law-insurance-backend-api-qa-903685040390.us-central1.run.app",
+    apiUrl: "https://money-law-gep-backend-api-qa-977990509270.us-central1.run.app",
     googleMapsApiKey: "AIzaSyBM4FFrc6lvc70H0HG6jgdSQMueH220R5k",
     featureFlags: {},
     receitawsApiUrl: "https://receitaws.com.br/v1/cnpj/",
